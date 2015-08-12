4cb08e5186d2f60dda1f6b124ad805398fc105aa
Thomas Watteyne <twatteyne@gmail.com>
2015-08-11 21:12:17 -0700

    dict_to_bin testing

--- sol.py
+++ sol.py
@@ -6,26 +6,26 @@ class Sol(object):
     
     #======================== public ==========================================
     
-    def dict_to_bin(o_dict):
+    def dict_to_bin(self,o_dict):
         '''
         \brief Convert list of dictionaries into a binary string.        
         '''
         raise NotImplementedError()
     
-    def bin_to_dict(o_bin):
+    def bin_to_dict(self,o_bin,mac=None):
         '''
         \brief Convert a binary string onto a list of dictionaries, each
             representing a sensor object.
         '''
         raise NotImplementedError()
     
-    def dict_to_json(o_dict,mode="verbose"):
+    def dict_to_json(self,o_dict,mode="verbose"):
         '''
         \brief Convert list of dictionaries into a JSON string.
         '''
         raise NotImplementedError()
 
-    def json_to_dict(o_json):
+    def json_to_dict(self,o_json):
         '''
         \brief Convert list of dictionaries into a JSON string.
         '''
--- tests/test_py_single.py
+++ tests/test_py_single.py
@@ -1,13 +1,43 @@
+import pytest
+import json
 
 #============================ defines ===============================
 
 #============================ fixtures ==============================
 
+SOL_EXAMPLE = [
+    {
+        'bin':
+            [
+                0<<6 | 0<<5 | 0<<4 | 0<<3 | 0<<0,    # header
+                0x11,0x22,0x33,0x44,                 # timestamp
+                0x55,                                # type
+                0x66,0x77                            # value
+            ],
+        'dict':
+            {
+                'mac':       [0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08],
+                'timestamp': 0x11223344,
+                'type':      0x55,
+                'value':     [0x66,0x77],
+            },
+        'json minimal': None,
+        'json verbose': None,
+    },
+]
+
+@pytest.fixture(params=SOL_EXAMPLE)
+def sol_example(request):
+    return json.dumps(request.param)
+
 #============================ helpers ===============================
 
 #============================ tests =================================
 
-def test_import():
-    import sys
-    print sys.path
+def test_dict_to_bin(sol_example):
+    sol_example = json.loads(sol_example)
+    
     import sol
+    sol = sol.Sol()
+    
+    assert sol.dict_to_bin(sol_example['dict'])==sol_example['bin']
