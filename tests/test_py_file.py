b55ddecd391228a14c1a970a36a159a855438bee
Thomas Watteyne <twatteyne@gmail.com>
2015-08-11 23:56:26 -0700

    test for retrieving corrupted data

--- tests/test_py_file.py
+++ tests/test_py_file.py
@@ -44,7 +44,7 @@ def test_dump_load(removeFile):
     sol = Sol.Sol()
     
     # prepare dicts to dump
-    dictsToDump = getRandomObjects(10000)
+    dictsToDump = getRandomObjects(1000)
     
     # dump
     sol.dumpToFile(dictsToDump,FILENAME)
@@ -54,3 +54,25 @@ def test_dump_load(removeFile):
     
     # compare
     assert dictsLoaded==dictsToDump
+
+def test_dump_corrupt_load(removeFile):
+    
+    import Sol
+    sol = Sol.Sol()
+    
+    # prepare dicts to dump
+    dictsToDump1 = getRandomObjects(500)
+    dictsToDump2 = getRandomObjects(500)
+    
+    # dump two sets of data with one corrupted record in the middle
+    sol.dumpToFile(dictsToDump1,FILENAME)
+    with open(FILENAME,'ab') as f:
+        bin = ''.join([chr(b) for b in [0x7E,0x10,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x00,0x00,0x00,0x00,0x75,0x94,0xE8,0x0B,0x6B,0xAE,0xE1,0x19,0x54,0x74,0xF3,0x00,0x00,0x7E]])
+        f.write(bin) 
+    sol.dumpToFile(dictsToDump2,FILENAME)
+    
+    # load
+    dictsLoaded = sol.loadFromFile(FILENAME)
+    
+    # compare
+    assert dictsLoaded==dictsToDump1+dictsToDump2
