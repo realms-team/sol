264162e41ac463ac082a36b5e9a36f744d39d18d
Thomas Watteyne <twatteyne@gmail.com>
2015-08-11 17:32:39 -0700

    version management

---
+++ sol-version.py
@@ -0,0 +1,21 @@
+import re
+
+SOL_VERSION = {}
+
+KEYS = ['SOL_VERSION_MAJOR','SOL_VERSION_MINOR','SOL_VERSION_PATCH','SOL_VERSION_BUILD']
+
+with open('sol-version.h') as f:
+    lines = f.readlines()
+    for k in KEYS:
+        for line in lines:
+            m = re.search('{0}\s*(\w)'.format(k), line)
+            if m:
+                SOL_VERSION[k] = m.group(1)
+
+for k in KEYS:
+    assert SOL_VERSION[k] != None
+
+if __name__=="__main__":
+    for k in KEYS:
+        print '{0}: {1}'.format(k,SOL_VERSION[k])
+    raw_input("Press Enter to close.")
