ee2fcb0fe0c18c8dd85ba9cf78c8de5bd5cc249e
Thomas Watteyne <twatteyne@gmail.com>
2015-08-11 18:14:44 -0700

    auto-testing

--- sol.py
+++ sol.py
@@ -6,19 +6,19 @@ class Sol(object):
     
     #======================== public ==========================================
     
+    def dict_to_bin(o_dict):
+        '''
+        \brief Convert list of dictionaries into a binary string.        
+        '''
+        raise NotImplementedError()
+    
     def bin_to_dict(o_bin):
         '''
         \brief Convert a binary string onto a list of dictionaries, each
             representing a sensor object.
         '''
         raise NotImplementedError()
-
-    def dict_to_bin(o_dict):
-        '''
-        \brief Convert list of dictionaries into a binary string.        
-        '''
-        raise NotImplementedError()
-
+    
     def dict_to_json(o_dict,mode="verbose"):
         '''
         \brief Convert list of dictionaries into a JSON string.
