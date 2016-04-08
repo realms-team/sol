a723bf8dd90c487a3e8cd8cfdc61a6bb19bd0ef0
Thomas Watteyne <twatteyne@gmail.com>
2016-04-08 20:00:50 -0300

    tests

--- tests/test_py_object_convertion.py
+++ tests/test_py_object_convertion.py
@@ -3,11 +3,9 @@ import json
 
 #============================ defines ===============================
 
-EXAMPLE_MAC = [0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08]
-
 #============================ fixtures ==============================
 
-SOL_EXAMPLE = [
+SOL_CHAIN_EXAMPLE = [
     {
         "dust": {
             "netTs":          1459833262,
@@ -23,7 +21,7 @@ SOL_EXAMPLE = [
                 "type"          : 39,
                 "value"         : [0, 0, 5, 0, 255, 1, 5, 0, 0, 0, 0, 61, 34, 104, 238, 0, 2, 56, 160, 0, 0, 117, 48, 1, 16, 9, 25]
             },
-        'bin macYES':
+        "bin":
             [
                 0<<6 | 0<<5 | 1<<4 | 0<<3 | 0<<0,          # header
                 0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,   # mac
@@ -34,56 +32,47 @@ SOL_EXAMPLE = [
                 0xee, 0x00, 0x02, 0x38, 0xa0, 0x00, 0x00,
                 0x75, 0x30, 0x01, 0x10, 0x09, 0x19         # value
             ],
-        'bin macNO':
-            [
-                0<<6 | 0<<5 | 0<<4 | 0<<3 | 0<<0,          # header
-                0x11,0x22,0x33,0x44,                       # timestamp
-                0x55,                                      # type
-                0x66,0x77                                  # value
-            ],
+        "http":
+            'TODO',
+        "influx":
+            'TODO',
     },
 ]
 
-@pytest.fixture(params=SOL_EXAMPLE)
-def sol_example(request):
+@pytest.fixture(params=SOL_CHAIN_EXAMPLE)
+def sol_chain_example(request):
     return json.dumps(request.param)
-
-JSON_MODE = ['minimal','verbose']
-
-@pytest.fixture(params=JSON_MODE)
-def json_mode(request):
-    return request.param
 
 #============================ helpers ===============================
 
 #============================ tests =================================
 
-def test_dust_to_json(sol_example):
-    sol_example = json.loads(sol_example)
+def test_dust_json_bin_http_bin_json_influx(sol_chain_example):
+    sol_chain_example = json.loads(sol_chain_example)
 
     import Sol
     sol = Sol.Sol()
 
-    # convert dust object
-    conv_obj = sol.dust_to_json(sol_example["dust"])
-
-    # update timestamp from JSON reference
-    json_obj = sol_example["json"]
-    json_obj['timestamp'] = conv_obj['timestamp']
-
-    # compare objects
-    assert conv_obj==json_obj
-
-def test_json_to_bin(sol_example):
-    sol_example = json.loads(sol_example)
-    import Sol
-    sol = Sol.Sol()
-
-    assert sol.json_to_bin([sol_example["json"]])==sol_example["bin macYES"]
-
-def test_bin_to_json(sol_example):
-    sol_example = json.loads(sol_example)
-    import Sol
-    sol = Sol.Sol()
-
-    assert sol.bin_to_json(sol_example["bin macYES"])==[sol_example["json"]]
+    # dust->json
+    sol_json  = sol.dust_to_json(sol_chain_example["dust"])
+    assert sol_json==sol_chain_example["json"]
+    
+    # json->bin
+    sol_bin   = sol.json_to_bin(sol_json)
+    assert sol_bin==sol_chain_example["bin"]
+    
+    # bin->http
+    sol_http  = sol.bin_to_http(sol_bin)
+    assert sol_http==sol_chain_example["http"]
+    
+    # http->bin
+    sol_bin   = sol.http_to_bin(sol_http)
+    assert sol_bin==sol_chain_example["bin"]
+    
+    # bin->json
+    sol_json  = sol.bin_to_json(sol_bin)
+    assert sol_json==sol_chain_example["json"]
+    
+    # json->influx
+    sol_inlux  = sol.bin_to_json(sol_json)
+    assert sol_influx==sol_chain_example["influx"]
