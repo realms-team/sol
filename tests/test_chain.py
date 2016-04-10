c3357b5ce6d2bb6db44bc7d847f83ae7bfed6767
Thomas Watteyne <twatteyne@gmail.com>
2016-04-10 12:34:54 -0300

    SOL_TYPE_DUST_EVENTMOTELOST passes

--- tests/test_chain.py
+++ tests/test_chain.py
@@ -314,7 +314,49 @@ SOL_CHAIN_EXAMPLE = [
             },
     },
     # SOL_TYPE_DUST_EVENTMOTELOST
-    # TODO
+    {
+        "dust":
+            "IpMgrConnectorMux.IpMgrConnectorMux.Tuple_eventMoteLost(     \
+                eventId      = 0x11223344,                                \
+                macAddress   = [1,1,1,1,1,1,1,1],                         \
+            )",
+        "json":
+            {
+                "timestamp"  : TIMESTAMP,
+                "mac"        : MACMANAGER,
+                "type"       : 0x1c,
+                "value"      : {
+                    'macAddress'  : [1,1,1,1,1,1,1,1],
+                },
+            },
+        "bin":
+            [
+                #ver   type   MAC    ts    typelen length
+                0<<6 | 0<<5 | 1<<4 | 0<<3 | 0<<2 | 3<<0,   # header
+                0x03,0x03,0x03,0x03,0x03,0x03,0x03,0x03,   # mac
+                0x05,0x05,0x05,0x05,                       # timestamp
+                0x1c,                                      # type
+                0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01,   # value
+            ],
+        "http":
+            '{                                                       \
+                "v" : 0,                                             \
+                "o" : [                                              \
+                    "EwMDAwMDAwMDBQUFBRwBAQEBAQEBAQ=="   \
+                ]                                                    \
+            }',
+        "influxdb":
+            {
+                "timestamp"  : TIMESTAMP,
+                "tag"        : {
+                    'mac'    : '03-03-03-03-03-03-03-03',
+                },
+                "measurement": 'SOL_TYPE_DUST_EVENTMOTELOST',
+                "fields"     : {
+                    'macAddress'  : '01-01-01-01-01-01-01-01',
+                },
+            },
+    },
     # SOL_TYPE_DUST_EVENTMOTEOPERATIONAL
     # TODO
     # SOL_TYPE_DUST_OAP_TEMPSAMPLE
