ea95fa87ba96b81517af3a0b9fb9b3eb314626da
Thomas Watteyne <twatteyne@gmail.com>
2016-04-10 09:50:39 -0300

    SOL_TYPE_DUST_NOTIF_EVENT_PATHCREATE test (which fails)

--- tests/test_chain.py
+++ tests/test_chain.py
@@ -5,9 +5,13 @@ from   SmartMeshSDK.IpMgrConnectorMux  import IpMgrConnectorMux
 
 #============================ defines ===============================
 
+MACMANAGER = [0xaa,0xbb,0xcc,0xdd,0xaa,0xbb,0xcc,0xdd]
+TIMESTAMP   = 0xddffddff
+
 #============================ fixtures ==============================
 
 SOL_CHAIN_EXAMPLE = [
+    # SOL_TYPE_DUST_NOTIF_DATA_RAW
     {
         "dust":
             "IpMgrConnectorMux.IpMgrConnectorMux.Tuple_notifData(    \
@@ -20,7 +24,7 @@ SOL_CHAIN_EXAMPLE = [
             )",
         "json":
             {
-                "timestamp"  : 0x11223344,
+                "timestamp"  : TIMESTAMP,
                 "mac"        : [1, 2, 3, 4, 5, 6, 7, 8],
                 "type"       : 0x0e,
                 "value"      : {
@@ -34,7 +38,7 @@ SOL_CHAIN_EXAMPLE = [
                 #ver   type   MAC    ts    typelen length
                 0<<6 | 0<<5 | 1<<4 | 0<<3 | 0<<2 | 3<<0,   # header
                 0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,   # mac
-                0x11,0x22,0x33,0x44,                       # timestamp
+                0xdd,0xff,0xdd,0xff,                       # timestamp
                 0x0e,                                      # type
                 0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,   # value
             ],
@@ -47,7 +51,7 @@ SOL_CHAIN_EXAMPLE = [
             }',
         "influxdb":
             {
-                "timestamp"  : 0x11223344,
+                "timestamp"  : TIMESTAMP,
                 "tag"        : {
                     'mac'    : '01-02-03-04-05-06-07-08',
                 },
@@ -59,6 +63,78 @@ SOL_CHAIN_EXAMPLE = [
                 },
             },
     },
+    # SOL_TYPE_DUST_NOTIF_HR_DEVICE
+    # TODO
+    # SOL_TYPE_DUST_NOTIF_HR_NEIGHBORS
+    # TODO
+    # SOL_TYPE_DUST_NOTIF_HR_DISCOVERED
+    # TODO
+    # SOL_TYPE_DUST_NOTIF_EVENT_PATHCREATE
+    {
+        "dust":
+            "IpMgrConnectorMux.IpMgrConnectorMux.Tuple_eventPathCreate(   \
+                eventId      = 0x11223344,                                \
+                source       = [1,1,1,1,1,1,1,1],                         \
+                dest         = [2,2,2,2,2,2,2,2],                         \
+                direction    = 3,                                         \
+            )",
+        "json":
+            {
+                "timestamp"  : TIMESTAMP,
+                "mac"        : MACMANAGER,
+                "type"       : 0x14,
+                "value"      : {
+                    'source'      : [1,1,1,1,1,1,1,1],
+                    'dest'        : [2,2,2,2,2,2,2,2],
+                    'direction'   : 3,
+                },
+            },
+        "bin":
+            [
+                #ver   type   MAC    ts    typelen length
+                0<<6 | 0<<5 | 1<<4 | 0<<3 | 0<<2 | 3<<0,   # header
+                0xaa,0xbb,0xcc,0xdd,0xaa,0xbb,0xcc,0xdd,   # mac
+                0xdd,0xff,0xdd,0xff,                       # timestamp
+                0x14,                                      # type
+                0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01,   # value
+                0x02,0x02,0x02,0x02,0x02,0x02,0x02,0x02,
+                0x03,
+            ],
+        "http":
+            '{                                             \
+                "v" : 0,                                   \
+                "o" : [                                    \
+                    "EwECAwQFBgcIESIzRA4BAgMEBQYHCA=="     \
+                ]                                          \
+            }',
+        "influxdb":
+            {
+                "timestamp"  : TIMESTAMP,
+                "tag"        : {
+                    'mac'    : 'aa-bb-cc-dd-aa-bb-cc-dd',
+                },
+                "measurement": 'SOL_TYPE_DUST_NOTIF_EVENT_PATHCREATE',
+                "fields"     : {
+                    'source'      : '01-01-01-01-01-01-01-01',
+                    'dest'        : '02-02-02-02-02-02-02-02',
+                    'direction'   : 3,
+                },
+            },
+    },
+    # SOL_TYPE_DUST_NOTIF_EVENT_PATHDELETE
+    # TODO
+    # SOL_TYPE_DUST_NOTIF_EVENT_MOTEJOIN
+    # TODO
+    # SOL_TYPE_DUST_NOTIF_EVENT_MOTECREATE
+    # TODO
+    # SOL_TYPE_DUST_NOTIF_EVENT_MOTEDELETE
+    # TODO
+    # SOL_TYPE_DUST_NOTIF_EVENT_MOTELOST
+    # TODO
+    # SOL_TYPE_DUST_NOTIF_EVENT_MOTEOPERATIONAL
+    # TODO
+    # SOL_TYPE_DUST_OAP_TEMPSAMPLE
+    # TODO
 ]
 
 @pytest.fixture(params=SOL_CHAIN_EXAMPLE)
@@ -76,7 +152,11 @@ def test_chain(sol_chain_example):
     sol = Sol.Sol()
 
     # dust->json
-    sol_json  = sol.dust_to_json(eval(sol_chain_example["dust"]),timestamp=0x11223344)
+    sol_json  = sol.dust_to_json(
+       eval(sol_chain_example["dust"]),
+       macManager  = MACMANAGER,
+       timestamp   = TIMESTAMP,
+    )
     assert sol_json==sol_chain_example["json"]
     
     # json->bin
