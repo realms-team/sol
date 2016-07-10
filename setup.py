5f4d121f47e48769bce2148f0e264b2c2314f6e9
Keoma Brun <keoma.brun@inria.fr>
2016-07-10 14:37:04 +0200

    #40 reading readme from setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,21 @@ from distutils.core import setup
 
 import solobjectlib.SolVersion as ver
 
+with open('README.md') as f:
+    readme = f.read()
+
+with open('license.txt') as f:
+    license = f.read()
+
 setup(
   name = 'solobjectlib',
-  packages = ['solobjectlib'], # this must be the same as the name above
+  packages = ['solobjectlib'],
   version = '.'.join(str(i) for i in ver.VERSION),
   description = 'The Sensor Object Library',
+  long_description=readme,
   author = 'Thomas Watteyne, Keoma Brun, Sami Malek, Ziran Zhang',
   author_email = 'keoma.brun@inria.fr',
-  url = 'https://github.com/realms-team/sol', # use the URL to the github repo
-  download_url = 'https://github.com/realms-team/sol/releases/tag/REL-0.0.5',
-  keywords = ['wireless', 'sensor', 'network'], # arbitrary keywords
-  classifiers = [],
+  url = 'https://github.com/realms-team/sol',
+  keywords = ['wireless', 'sensor', 'network'],
+  licence=license
 )
