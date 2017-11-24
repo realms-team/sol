1c133e6c2ff6578d6cb7107d1a1331c76e8415c3
Keoma Brun <keoma.brun@inria.fr>
2017-11-24 10:46:12 +0100

    #70 fix

--- solobjectlib/Sol.py
+++ solobjectlib/Sol.py
@@ -6,10 +6,7 @@ import sys
 import os
 
 here = os.path.dirname(__file__)
-if __name__ == "__main__":
-    sys.path.insert(0, os.path.join(here, '..', '..', 'smartmeshsdk', 'libs'))
-else:
-    sys.path.insert(0, os.path.join(here, '..', '..', 'smartmeshsdk', 'libs'))
+sys.path.insert(0, os.path.join(here, '..', '..', 'smartmeshsdk', 'libs'))
 
 # =========================== imports =========================================
 
