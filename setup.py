85d81e462186a0e9deeee2fefe62a37e0f446044
Keoma Brun <keoma.brun@inria.fr>
2018-05-03 15:26:09 +0200

    to 1.7.0.0

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-import solobjectlib.SolVersion as ver
+import sensorobjectlibrary.Sol as sol
 
 with open('README.md') as f:
     readme = f.read()
@@ -9,14 +9,14 @@ with open('license.txt') as f:
     license = f.read()
 
 setup(
-  name = 'solobjectlib',
-  packages = ['solobjectlib'],
-  version = '.'.join(str(i) for i in ver.VERSION),
-  description = 'The Sensor Object Library',
-  long_description=readme,
-  author = 'Thomas Watteyne, Keoma Brun, Sami Malek, Ziran Zhang',
-  author_email = 'keoma.brun@inria.fr',
-  url = 'https://github.com/realms-team/sol',
-  keywords = ['wireless', 'sensor', 'network'],
-  licence=license
+    name = 'sensorobjectlibrary',
+    packages = ['sensorobjectlibrary'],
+    version = '.'.join(str(i) for i in sol.VERSION),
+    description = 'The Sensor Object Library',
+    long_description=readme,
+    author = 'Thomas Watteyne, Keoma Brun, Sami Malek, Ziran Zhang',
+    author_email = 'keoma.brun@inria.fr',
+    url = 'https://github.com/realms-team/sol',
+    keywords = ['wireless', 'sensor', 'network'],
+    licence=license
 )
